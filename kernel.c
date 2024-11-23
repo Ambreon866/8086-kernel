6436ae2c5ccc3cfea93ca6b3217efed007c27869
Ambreon866 <pronin.sa48@gmail.com>
2024-11-23 10:49:12 +0300

    прототип ядра

---
+++ kernel.c
@@ -0,0 +1,24 @@
+// Определяем адрес видеопамяти
+#define VIDEO_MEMORY 0xB8000
+#define WHITE_ON_BLACK 0x07
+
+// Функция для вывода строки на экран
+void print_string(const char *str) {
+    char *vidptr = (char *)VIDEO_MEMORY; // Указатель на видеопамять
+    unsigned int i = 0;
+
+    // Заполняем видеопамять
+    while (str[i] != '\0') {
+        vidptr[i * 2] = str[i];      // Символ
+        vidptr[i * 2 + 1] = WHITE_ON_BLACK; // Атрибут (светлый текст на темном фоне)
+        i++;
+    }
+}
+
+// Основная функция ядра
+void kernel_main() {
+    print_string("Hello from the kernel!");
+
+    // Бесконечный цикл
+    while (1) {}
+}
